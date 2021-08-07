ce0107ed87d879005f98e7578a3448738b19b9e8
Stephanie <Rinehart>
2021-08-06 21:36:24 -0400

    add navigation and about sub

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,17 @@
 import React from "react";
 import Navbar from "./navbar/navbar";
-import Content from "../components/content/content";
 import Header from "../components/header/header"
+import Contact from "../components/contact/contact"
+import Content from "../components/content/content";
 import "./layout.scss"; 
 
 export default function Layout({ pageMeta, children }) {
     return (
         <>
         <div className="container">
+          <Navbar />
           <Header />
+          <Contact />
           {/* <Navbar /> */}
           {/* <Content> */}
             {children}
