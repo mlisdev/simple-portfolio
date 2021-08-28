af4798af0160b297069c4e4e8efb6b2eb11dafe6
Stephanie <Rinehart>
2021-08-28 18:44:43 -0400

    forgotten push

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,71 +1,17 @@
 import React from "react";
-import { useStaticQuery, graphql } from "gatsby";
-import { GatsbyImage } from "gatsby-plugin-image";
 import "./about.scss"; 
 
 const About = () => {
-  // const data = useStaticQuery(
-  //   graphql`
-  //     query MyQuery {
-  //       allDatoCmsProjectContent {
-  //         nodes {
-  //           id
-  //           githublink
-  //           description {
-  //             value
-  //           }
-  //           link
-  //           originalId
-  //           title
-  //           technologies {
-  //             value
-  //           }
-  //         }
-  //       }
-  //     }
-  //   `
-  // )
-  
-  const aboutData = useStaticQuery(
-    graphql`
-      query About{
-  allDatoCmsAboutPage {
-    nodes {
-      aboutHeadline
-      aboutText {
-        value
-      }
-      aboutImg {
-        alt
-        gatsbyImageData(placeholder: BLURRED, width: 600, forceBlurhash: false)
-      }
-    }
-  }
-}
-      `
-  )
-
   return (
     <>
       <div className="about" id="about">
       <div className="about-photo">
         <center>
-            <GatsbyImage image={aboutData.allDatoCmsAboutPage.aboutImg.gatsbyImageData} />
-            </center> 
-          </div>
-          <div className="about-text">
-          <h1>About Steph</h1>
-
-              </div>
-      {/* {data.allDatoCmsProjectContent.nodes.map((proj) => {
-        return(
-        <>
-        {proj.title}
-          </>
-        )
-        }
-      )
-    }  */}
+        </center> 
+      </div>
+      
+      <div className="about-text">
+      </div>
       </div>
     </>  
         );
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,47 @@
 import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
 import Navbar from "./navbar/navbar";
 import Header from "../components/header/header"; 
 import About from "../components/about/about"; 
 import "./layout.scss"; 
 
 export default function Layout({ pageMeta, children }) {
+    const queryData = useStaticQuery(
+      graphql`
+        query MyQuery {
+          allDatoCmsAboutPage {
+              nodes {
+              aboutHeadline
+              aboutText {
+                  value
+              }
+              aboutImg {
+                  gatsbyImageData(placeholder: BLURRED, width: 600, forceBlurhash: false)
+                  alt
+              }
+              }
+          }
+          allDatoCmsProjectContent {
+              nodes {
+              githublink
+              id
+              images {
+                  gatsbyImageData
+              }
+              link
+              technologies {
+                  value
+              }
+              title
+              description {
+                  value
+              }
+              }
+      }
+      }
+      `
+    )
+    
     return (
         <>
         <div className="container">
