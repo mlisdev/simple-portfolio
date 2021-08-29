770c2342b47309bf10d1694aa1f3afec15532c58
Stephanie <Rinehart>
2021-08-29 13:50:02 -0400

    layout updates, add about styling

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,17 +1,26 @@
 import React from "react";
-import "./about.scss"; 
+import "./about.scss";
+import { StaticImage } from "gatsby-plugin-image"
+import stephanie from "../../images/stephanie.jpg"
 
 const About = () => {
   return (
     <>
       <div className="about" id="about">
-      <div className="about-photo">
-        <center>
+        <div className="about-col" id="about-text">
+          <h1>the short : </h1><br />
+          
+
+      </div>
+      <div className="about-col" id="about-photo">
+          <center>
+            <img
+              // src="../../images/stephanie.jpg"
+              src={stephanie} />
         </center> 
       </div>
       
-      <div className="about-text">
-      </div>
+
       </div>
     </>  
         );
